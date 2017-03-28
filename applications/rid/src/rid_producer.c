bdb24b180f6b9c8876c61b5b4546de4d6720ede8
Damiao Rodrigues <adamiaonr@gmail.com>
2017-03-28 18:03:32 +0100

    forgot to add RID application examples

---
+++ applications/rid/src/rid_producer.c
@@ -0,0 +1,445 @@
+/* ts=4 */
+/*
+** Copyright 2015 Carnegie Mellon University
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**    http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <sys/param.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/file.h>
+
+#include "Xsocket.h"
+#include "dagaddr.hpp"
+#include "rid.h"
+#include "xcache.h"
+
+#define VERSION "v0.1"
+#define TITLE "XIA RID Producer Application"
+
+// XXX: 0 = 'permanent', i.e. cached objects live forever
+#define DEFAULT_CACHE_SLICE_TTL     (unsigned) 0
+#define DEFAULT_CACHE_SLICE_SIZE    (unsigned) 200000000
+
+// the name and SID used to contect the producer directly: i.e. 
+// using SID forwarding (e.g. via fallback), not via RIDs
+#define ORIGIN_SERVER_NAME  "www_s.dgram_producer.aaa.xia"
+#define ORIGIN_SERVER_SID   "SID:0f00000000000000000000000000000000008888"
+
+#define MAX_DAG_SIZE    (int) (4 * 4 + (XID_SIZE * 2) + 1)
+
+char PRODUCER_AID[4 + (XID_SIZE * 2) + 1];
+char PRODUCER_HID[4 + (XID_SIZE * 2) + 1];
+char PRODUCER_4ID[4 + (XID_SIZE * 2) + 1];
+
+/*
+** write the message to stdout unless in quiet mode
+*/
+void say(const char * fmt, ...)
+{
+    va_list args;
+
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    va_end(args);
+}
+
+/*
+** always write the message to stdout
+*/
+void warn(const char * fmt, ...)
+{
+    va_list args;
+
+    va_start(args, fmt);
+    vfprintf(stdout, fmt, args);
+    va_end(args);
+}
+
+/*
+** write the message to stdout, and exit the app
+*/
+void die(int ecode, const char * fmt, ...)
+{
+    va_list args;
+
+    va_start(args, fmt);
+    vfprintf(stdout, fmt, args);
+    va_end(args);
+    fprintf(stdout, "[rid_producer]: exiting\n");
+    exit(ecode);
+}
+
+int send_rid_response(
+        XcacheHandle * xcache_handle,
+        sockaddr_x * rid_req_src,
+        socklen_t rid_req_src_len) {
+
+    // what does send_rid_response() do?
+    //     1) generate a sensor value
+    //    2) send it to rid_req_src using xcache's interfaces
+
+    int rc = 0;
+
+    // 1) generate a sensor value, save it in value_str according to a some
+    // format
+    char value_str[128] = {'\0'};
+    snprintf(
+            value_str,
+            128,
+            "timestamp:%u:temperature:%d:unit:degC",
+            (unsigned) time(NULL),
+            (rand() % 111 + (-50)));
+
+    // 2) send the value to rid_req_src, using xcache's XpushChunkto
+    sockaddr_x cid_value_addr;
+
+    if ((rc = XpushChunkto(
+            xcache_handle,
+            value_str,
+            strlen(value_str),
+            &cid_value_addr,
+            rid_req_src,
+            rid_req_src_len)) < 0) {
+
+        warn("[rid_producer]: XpushChunkto() error = %d", errno);
+
+    } else {
+
+        say("[rid_producer]: 'Xpushed' RID response:"\
+                "\n\t[DST_DAG] = %s"\
+                "\n\t[RID PAYLOAD] = %s"\
+                "\n\t[CID PAYLOAD] = %s\n",
+                Graph((sockaddr_x *) rid_req_src).dag_string().c_str(),
+                Graph(&cid_value_addr).dag_string().c_str(),
+                value_str);
+    }
+
+    return rc;
+}
+
+int setup_rid_socket(char * rid_string)
+{
+    int rid_sock = 0;
+
+    if ((rid_sock = Xsocket(AF_XIA, SOCK_DGRAM, 0)) < 0) {
+        die(-1, "[rid_producer]: Xsocket(SOCK_DGRAM) error = %d", errno);
+    }
+
+    char localhost_dag_str[MAX_DAG_SIZE] = { 0 };
+    char localhost_4id_str[MAX_DAG_SIZE] = { 0 };
+    
+    if (XreadLocalHostAddr(
+            rid_sock,
+            localhost_dag_str, MAX_DAG_SIZE,
+            localhost_4id_str, MAX_DAG_SIZE) < 0 ) {
+
+        die(    -1,
+                "[rid_producer]: error reading localhost address\n");
+    }
+
+    // create a local RID DAG, in the style AD:HID:RID (= rid_string).
+    sockaddr_x rid_local_addr;
+    socklen_t rid_local_addr_len;
+
+    to_rid_addr(
+        rid_string,
+        localhost_dag_str,
+        &rid_local_addr,
+        &rid_local_addr_len);
+
+    // 3.3) bind rid_sock to rid_local_addr
+    if (Xbind(
+            rid_sock,
+            (struct sockaddr *) &rid_local_addr,
+            rid_local_addr_len) < 0) {
+
+        Xclose(rid_sock);
+
+        die(    -1,
+                "[rid_producer]: unable to Xbind() to DAG %s error = %d\n",
+                Graph(&rid_local_addr).dag_string().c_str(),
+                errno);
+    }
+
+    say("[rid_producer]: will listen to RID packets directed at: \n%s\n\n",
+        Graph(&rid_local_addr).dag_string().c_str());
+
+    return rid_sock;
+}
+
+int setup_sid_socket()
+{
+    int sid_sock;
+
+    if ((sid_sock = Xsocket(AF_XIA, SOCK_DGRAM, 0)) < 0) {
+        die(-1, "[rid_producer::]: Xsocket(SOCK_DGRAM) error = %d", errno);
+    }
+
+    // we want to associate PRODUCER_NAME to a DAG with the following 
+    // structure:
+    //
+    //  DAG 2 0 - 
+    //  AD:     <local AD> 2 1 - 
+    //  HID:    <local HID> 2 - 
+    //  SID:    <ORIGIN_SERVER_SID>
+    //
+    // we do it via Xgetaddrinfo()
+    struct addrinfo * local_addr_info;
+    if (Xgetaddrinfo(NULL, ORIGIN_SERVER_SID, NULL, &local_addr_info) != 0)
+        die(-1, "[rid_producer]: Xgetaddrinfo() error\n");
+
+    sockaddr_x * listen_addr = (sockaddr_x *) local_addr_info->ai_addr;
+
+    // print the generated DAG
+    printf("[rid_producer]: binding RID producer to (secondary) DAG:\n%s\n\n", 
+        Graph(listen_addr).dag_string().c_str());
+
+    // associate the name PRODUCER_NAME to the DAG address
+    if (XregisterName(ORIGIN_SERVER_NAME, listen_addr) < 0 )
+        die(-1, "[rid_producer]: error registering name: %s\n", ORIGIN_SERVER_NAME);
+    say("[rid_producer]: producer registered name: \n%s\n\n", ORIGIN_SERVER_NAME);
+
+    // finally, bind sid_socket to the generated DAG
+    if (Xbind(
+            sid_sock,
+            (struct sockaddr *) listen_addr,
+            sizeof(*listen_addr)) < 0) {
+
+        Xclose(sid_sock);
+
+        die(    -1,
+                "[rid_producer]: unable to Xbind() to DAG %s error = %d",
+                Graph(listen_addr).dag_string().c_str(),
+                errno);
+    }
+
+    return sid_sock;
+}
+
+int main(int argc, char **argv)
+{
+    // rid producer listens to RID requests and direct SID queries
+    int rid_sock = 0, sid_sock = 0;
+
+    char * name = (char *) calloc(PREFIX_MAX_LENGTH, sizeof(char));
+    char * rid_string;
+
+    // similarly to other XIA example apps, print some initial info on the app
+    say("\n%s (%s): started\n\n", TITLE, VERSION);
+
+    // 1) gather the application arguments
+
+    // if not enough arguments, stop immediately...
+    if (argc < 3) {
+        die(-1, "[rid_producer]: usage: rid_producer -n <URL-like (producer) name>\n");
+    }
+
+    char **av = argv;
+    argc--, av++;
+
+    while (argc > 0 && *av[0] == '-') {
+
+        int c = argc;
+
+        while (*++av[0]) {
+
+            if (c != argc)
+                break;
+
+            switch (*av[0]) {
+                // 1.2) single URL-like name given as parameter to generate
+                // RIDs (e.g. '/cmu/ece/rmcic/2221c/sensor/temperature/')
+                case 'n':
+                    // 1.2.1) shift to the argument value, save the URL-like
+                    // name in prefix
+                    argc--, av++;
+                    strncpy(name, av[0], strlen(av[0]));
+
+                    say("[rid_producer]: user supplied prefix %s\n",
+                        name);
+
+                    break;
+
+                // 1.3) file with list of URL-like names to generate RIDs
+                // FIXME: will be used when binding to multiple RIDs is
+                // implemented
+                // case 'f':
+                //     argc--, av++;
+                //     //npackets = atoi(av[0]);
+                //     break;
+            }
+        }
+
+        argc--, av++;
+    }
+
+    // 1.4) create RID to be served by this producer
+    if (name[0] != '\0') {
+
+        rid_string = name_to_rid(name);
+
+        say("[rid_producer]: created RID %s out of prefix %s\n",
+            rid_string,
+            name);
+
+    } else {
+
+        // 1.4.1) if no name has been specified, no point going on...
+        die(-1, "[rid_producer]: name string is empty\n");
+    }
+
+    // 2) initialize the producer's xcache handle
+    say("[rid_producer]: initializing xcache handle\n");
+    XcacheHandle xcache_handle; 
+    XcacheHandleInit(&xcache_handle);
+    say("[rid_producer]: done!\n");
+
+    // 3) setup RID and SID sockets
+    rid_sock = setup_rid_socket(rid_string);
+    sid_sock = setup_sid_socket();
+
+    // 4) select() monitors both rid_sock and sid_sock
+
+    // create variables to hold src and dst addresses of the 
+    // RID request. src will be directly set by Xrevfrom(), while dst 
+    // is implicit (the x_sock socket should only get requests for the 
+    // prefix bound to the socket)
+    sockaddr_x rid_req_src, sid_req_src;
+    socklen_t rid_req_src_len, sid_req_src_len;
+
+    // a buffer for the whole RID request (have no idea what it should
+    // contain for now)
+    unsigned char rid_req[RID_MAX_PACKET_SIZE], sid_req[RID_MAX_PACKET_SIZE];
+    int rid_req_len = sizeof(rid_req), sid_req_len = sizeof(sid_req);
+
+    // create a file descriptor set
+    fd_set socket_fds;
+    // max fd required for select() call
+    int max_sock = 0;
+    if (rid_sock > sid_sock)
+        max_sock = rid_sock;
+    else
+        max_sock = sid_sock;
+
+    // set a small timeout for the select() call (e.g. 500 ms)
+    struct timeval timeout;
+
+    int rc = 0;
+    while(1) {
+
+        // you need to 're-arm' timeout and socket_fds every time (Xselect() 
+        // alters these):
+
+        // check what's up with the sockets
+        timeout.tv_sec = 0;
+        timeout.tv_usec = 500000;
+        // clear socket_fds, then add both rid_sock and sid_sock to it
+        FD_ZERO(&socket_fds);
+        FD_SET(rid_sock, &socket_fds);
+        FD_SET(sid_sock, &socket_fds);
+
+        rc = Xselect(max_sock + 1, &socket_fds, 0, 0, &timeout);
+
+        if (rc == 0) {
+
+            // say("[rid_producer]: Xselect() timeout\n");
+            continue;
+
+        } else if (rc > 0) {
+
+            say("[rid_producer]: Xselect() has something...\n");
+
+            // check rid_sock and sid_sock separately
+            if (FD_ISSET(rid_sock, &socket_fds)) {
+
+                if ((rc = Xrecvfrom(
+                        rid_sock,
+                        rid_req,
+                        rid_req_len,
+                        0,
+                        (struct sockaddr *) &rid_req_src,
+                        &rid_req_src_len)) < 0) {
+
+                    if (errno == EINTR)
+                        continue;
+
+                    warn("[rid_producer]: Xrecvfrom() error = %d", errno);
+                        continue;
+                }
+
+                say("[rid_producer]: got RID request from: \n%s\n\n",
+                        Graph((sockaddr_x *) &rid_req_src).dag_string().c_str());
+
+                send_rid_response(
+                        &xcache_handle,
+                        &rid_req_src,
+                        rid_req_src_len);
+            }
+
+            if (FD_ISSET(sid_sock, &socket_fds)) {
+
+                if ((rc = Xrecvfrom(
+                        sid_sock,
+                        sid_req,
+                        sid_req_len,
+                        0,
+                        (struct sockaddr *) &sid_req_src,
+                        &sid_req_src_len)) < 0) {
+
+                    if (errno == EINTR)
+                        continue;
+
+                    warn("[rid_producer]: Xrecvfrom() error = %d", errno);
+                        continue;
+                }
+
+                say("[rid_producer]: got SID request from:"\
+                        "\n\t[SRC_DAG] = %s",
+                        Graph((sockaddr_x *) &sid_req_src).dag_string().c_str());
+
+                // TODO: the application should also be able to verify other issues
+                // e.g. if the requested name in its `original' form, etc. to
+                // distinguish BF false positives (FPs). this information should
+                // somehow be included in an RID extension header (e.g. like
+                // a ContentHeader).
+                send_rid_response(
+                        &xcache_handle,
+                        &sid_req_src,
+                        sid_req_src_len);
+            }
+
+        } else {
+
+            die(-1, "[rid_producer]: Xselect() rc = %d, error = %d (%s)\n", rc, errno, strerror(errno));
+        }
+    }
+
+    // ************************************************************************
+    // 5) close everything
+    // ************************************************************************
+    say("[rid_producer]: this is rid_requester, signing off...");
+
+    Xclose(rid_sock);
+    Xclose(sid_sock);
+
+    free(name);
+    free(rid_string);
+
+    exit(rc);
+}
---
+++ applications/rid/src/rid_requester.c
@@ -0,0 +1,360 @@
+/* ts=4 */
+/*
+** Copyright 2015 Carnegie Mellon University
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**    http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include "rid.h"
+#include "xcache.h"
+#include "dagaddr.hpp"
+
+#define VERSION "v0.1"
+#define TITLE "XIA RID Requester Application"
+
+#define RID_REQUEST_DEFAULT_AD     (char *) "1000000000000000000000000000000000000002"
+#define RID_REQUEST_DEFAULT_HID    (char *) "0000000000000000000000000000000000000005"
+#define SID_REQUESTER       "SID:00000000dd41b924c1001cfa1e1117a812492434"
+#define ORIGIN_SERVER_NAME  "www_s.dgram_producer.aaa.xia"
+
+/*
+** write the message to stdout unless in quiet mode
+*/
+void say(const char * fmt, ...)
+{
+    va_list args;
+
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    va_end(args);
+}
+
+/*
+** always write the message to stdout
+*/
+void warn(const char * fmt, ...)
+{
+    va_list args;
+
+    va_start(args, fmt);
+    vfprintf(stdout, fmt, args);
+    va_end(args);
+}
+
+/*
+** write the message to stdout, and exit the app
+*/
+void die(int ecode, const char * fmt, ...)
+{
+    va_list args;
+
+    va_start(args, fmt);
+    vfprintf(stdout, fmt, args);
+    va_end(args);
+    fprintf(stdout, "[rid]: exiting\n");
+    exit(ecode);
+}
+
+int setup_sid_sock() {
+
+    int x_sock = 0;
+
+    if ((x_sock = Xsocket(AF_XIA, SOCK_DGRAM, 0)) < 0) {
+        die(-1, "[rid_requester]: Xsocket(SOCK_DGRAM) error = %d", errno);
+    }
+
+    // FIXME: will use Xgetaddrinfo(name = NULL, ...) to build
+    // AD:HID:SIDx (= SID_REQUESTER) with local AD:HID
+    struct addrinfo * local_addr_info;
+
+    if (Xgetaddrinfo(
+            NULL, 
+            (const char *) SID_REQUESTER, 
+            NULL, 
+            &local_addr_info) != 0) {
+
+        die(-1, "[rid_requester]: Xgetaddrinfo() error");
+    }
+
+    sockaddr_x * listen_addr = (sockaddr_x *) local_addr_info->ai_addr;
+
+    // bind the socket to the local DAG
+    if (Xbind(
+            x_sock,
+            (struct sockaddr *) listen_addr,
+            sizeof(*listen_addr)) < 0) {
+
+        Xclose(x_sock);
+
+        die(    -1,
+                "[rid_requester]: unable to Xbind() to DAG %s error = %d",
+                Graph(listen_addr).dag_string().c_str(),
+                errno);
+    }
+
+    say("[rid_requester]: will listen to SID packets directed at: \n%s\n\n",
+            Graph(listen_addr).dag_string().c_str());
+
+    return x_sock;
+}
+
+int feth_remote_dag(const char * origin_server_name, Graph & remote_dag) {
+
+    struct addrinfo * remote_address_info;
+    sockaddr_x * remote_socket_address;
+
+    if (Xgetaddrinfo(
+        origin_server_name, 
+        NULL, 
+        NULL, 
+        &remote_address_info) != 0) {
+
+        say("[rid_requester] unable to lookup name %s\n", origin_server_name);
+
+        return -1;
+    }
+
+    remote_socket_address = (sockaddr_x *) remote_address_info->ai_addr;
+
+    remote_dag = Graph(remote_socket_address);
+    say("[rid_requester] remote DAG for name %s is:\n%s\n\n", 
+        origin_server_name, remote_dag.dag_string().c_str());
+
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    // file descriptors for 'SOCK_DGRAM' Xsocket:
+    //    -# 'SOCK_DGRAM': send RID request and listen for responses
+    int x_sock = 0;
+
+    char * name = (char *) calloc(PREFIX_MAX_LENGTH, sizeof(char));
+    char * rid_string;
+
+    // similarly to other XIA example apps, print some initial info on the app
+    say("\n%s (%s): started\n", TITLE, VERSION);
+
+    // 1) gather the application arguments
+
+    // if not enough arguments, stop immediately...
+    if (argc < 3) {
+        die(-1, "usage: rid_requester -n <URL-like name>\n");
+    }
+
+    char **av = argv;
+    argc--, av++;
+
+    while (argc > 0 && *av[0] == '-') {
+
+        int c = argc;
+
+        while (*++av[0]) {
+
+            if (c != argc)
+                break;
+
+            switch (*av[0]) {
+                // single URL-like name given as parameter to generate
+                // RIDs (e.g. uof/cs/r9x/r9001/)
+                case 'n':
+                    // 1.1.1) shift to the argument value, save the url-like
+                    // name
+                    argc--, av++;
+                    strncpy(name, av[0], strlen(av[0]));
+                    break;
+
+                // file with list of URL-like names to generate RIDs for
+                // request
+                // case 'f':
+                //     argc--, av++;
+                //     //npackets = atoi(av[0]);
+                //     break;
+            }
+        }
+
+        argc--, av++;
+    }
+
+    // 2) create RID out of the provided name
+    if (name[0] != '\0') {
+
+        rid_string = name_to_rid(name);
+        say("[rid_requester]: created RID %s out of name %s\n",
+            rid_string,
+            name);
+
+    } else {
+
+        // if no name has been specified, no point going on...
+        die(-1, "[rid_requester]: name string is empty\n");
+    }
+
+    // 3) initialize the requester's xcache handle
+    say("[rid_requester]: initializing xcache handle...\n");
+    XcacheHandle xcache_handle; 
+    XcacheHandleInit(&xcache_handle);
+    say("[rid_requester]: ... done!\n");
+
+    // 4) open a 'SOCK_DGRAM' Xsocket, make it listen on SID_REQUESTER, on 
+    // which it will listen for RID responses
+    x_sock = setup_sid_sock();
+
+    // 5) send the RID request
+    int rid_packet_len = 0;
+    sockaddr_x rid_dest_addr;
+    socklen_t rid_dest_addr_len;
+
+    // get the AD and HID of origin server
+    Graph remote_dag;
+    if (feth_remote_dag(ORIGIN_SERVER_NAME, remote_dag) < 0) {
+
+        Node n_src, n_ad(XID_TYPE_AD, RID_REQUEST_DEFAULT_AD), n_hid(XID_TYPE_HID, RID_REQUEST_DEFAULT_HID);
+        remote_dag = n_src * n_ad * n_hid;
+    }
+
+    to_rid_addr(
+        rid_string, 
+        (char *) remote_dag.dag_string().c_str(),
+        &rid_dest_addr,
+        &rid_dest_addr_len);
+
+    // 3.3.1) TODO: what should be set in the payload? send the 
+    // full name for now...
+    char rid_packet_payload[RID_MAX_PACKET_SIZE] = {'\0'}; 
+    strncpy(rid_packet_payload, name, RID_MAX_PACKET_SIZE);
+    rid_packet_len = strlen(rid_packet_payload);
+
+    say("[rid_requester]: sending RID request:"\
+            "\n\t[DST_DAG] = %s"\
+            "\n\t[PAYLOAD] = %s\n",
+            Graph(&rid_dest_addr).dag_string().c_str(),
+            rid_packet_payload);
+    
+    int rc = 0;
+
+    rc = Xsendto(
+            x_sock,
+            rid_packet_payload,
+            rid_packet_len,
+            0,
+            (struct sockaddr *) &rid_dest_addr,
+            rid_dest_addr_len);
+
+    // 3.2) check status of RID request
+    if (rc < 0 || rc != rid_packet_len) {
+
+        if (rc < 0) {
+            die(-1, "[rid_requester]: Xsendto() error = %d", errno);
+        }
+
+        printf("[rid_requester]: sent RID request:"\
+                "\n\t[DST_DAG] = %s"\
+                "\n\t[PAYLOAD] = %s"\
+                "\n\t[RETURN_CODE] = %d\n",
+                Graph(&rid_dest_addr).dag_string().c_str(),
+                rid_packet_payload,
+                rc);
+
+        fflush(stdout);
+    }
+
+    // 4) listen to responses to the RID request 
+
+    // 4.4) start listening to RID responses
+    // FIXME: ... for now, in an endless loop. this is likely to change in the
+    // future after i receive some feedback from prs on the RID protocols
+    sockaddr_x rid_resp_src;
+    socklen_t rid_resp_src_len = sizeof(sockaddr_x);
+
+    // a buffer for the RID response(s)
+    char rid_resp[RID_MAX_PACKET_SIZE]; 
+    char * xfetch_chunk_buf;
+    int rid_resp_len = sizeof(rid_resp);
+
+    while (1) {
+
+        say("[rid_requester]: listening for new responses...\n");
+
+        int bytes_rcvd = -1;
+
+        // cleanup rid_resp just in case...
+        memset(rid_resp, 0, RID_MAX_PACKET_SIZE);
+
+        // 4.4.1) Xrecvfrom() blocks while waiting for something to
+        // appear on x_sock
+        if ((bytes_rcvd = Xrecvfrom(
+                                x_sock,
+                                rid_resp,
+                                rid_resp_len,
+                                0,
+                                (struct sockaddr *) &rid_resp_src,
+                                &rid_resp_src_len)) < 0) {
+
+            die(-1, "[rid_requester]: Xrecvfrom() error = %d", errno);
+
+        } else {
+
+            say("[rid_requester]: received RID response: "\
+                    "\n\t[SRC. ADDR]: %s"\
+                    "\n\t[PAYLOAD]: %s"\
+                    "\n\t[SIZE]: %d\n",
+                    Graph(&rid_resp_src).dag_string().c_str(),
+                    rid_resp,
+                    rid_resp_len);
+
+            // 4.3) use the CID given in the payload to fetch content from the 
+            // local cache, using xcache interfaces
+            sockaddr_x cid_resp_addr;
+            socklen_t cid_resp_addr_len;
+
+            to_cid_addr(rid_resp, &cid_resp_addr, &cid_resp_addr_len);
+
+            // cleanup rid_resp just in case...
+            memset(rid_resp, 0, RID_MAX_PACKET_SIZE);
+
+            // 4.4) use XfetchChunk() to get the CID's content
+            if (XfetchChunk(
+                            &xcache_handle, 
+                            (void **) &xfetch_chunk_buf, XCF_BLOCK, 
+                            &cid_resp_addr, cid_resp_addr_len) < 0) {
+
+                warn("[rid_requester]: XfetchChunk() error = %d", errno);
+
+            } else {
+
+                say("[rid_requester]: contents of included CID: "\
+                        "\n\t[CID]: %s"\
+                        "\n\t[CONTENT]: %s\n",
+                        Graph(&cid_resp_addr).dag_string().c_str(),
+                        xfetch_chunk_buf);
+            }
+        }
+    }
+
+    // ************************************************************************
+    // 5) close everything
+    // ************************************************************************
+    say("[rid_requester]: this is rid_requester, signing off...");
+
+    Xclose(x_sock);
+
+    free(name);
+    free(rid_string);
+
+    exit(rc);
+}
