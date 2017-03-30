fc944d99cdb534d1b18480654c347a671aa599ea
Damiao Rodrigues <adamiaonr@gmail.com>
2017-03-30 23:25:01 +0100

    further changes to xbind()... this is embarassing...

--- click/elements/xia/xtransport.cc
+++ click/elements/xia/xtransport.cc
@@ -1481,6 +1481,8 @@ void XTRANSPORT::Xbind(unsigned short _sport, uint32_t id, xia::XSocketMsg *xia_
 		sk->initialized = true;
 		sk->port = _sport;
 
+		click_chatter("XTRANSPORT::Xbind() : in src_path.parse(sdag_string)");
+
 		//Check if binding to full DAG or just to SID only
 		if (sk->src_path.first_hop_is_sid()) {
 			sk->full_src_dag = false;
